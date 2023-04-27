df38139a1dde4c0b3fad9af24986fb1961a7d4de
raoulbia <raoulbia@gmail.com>
2023-04-27 16:52:17 +0100

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,10 +22,8 @@ AUTHOR_FEED_RSS = None
 # all defaults to True.
 DISPLAY_HEADER = True
 DISPLAY_FOOTER = True
-DISPLAY_HOME   = False
-DISPLAY_MENU   = False
-DISPLAY_PAGES_ON_MENU = False
-DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_HOME   = True
+DISPLAY_MENU   = True
 
 # provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
 #TAGS_URL           = 'tags'
@@ -36,15 +34,12 @@ DISPLAY_CATEGORIES_ON_MENU = False
 #CATEGORIES_SAVE_AS = 'categories/index.html'
 ARCHIVES_URL       = 'archives'
 ARCHIVES_SAVE_AS   = 'archives/index.html'
-HOMEPAGE_URL       = 'homepage'
-HOMEPAGE_SAVE_AS   = 'homepage/index.html'
 
 # use those if you want pelican standard pages to appear in your menu
 MENU_INTERNAL_PAGES = (
     #('Tags', TAGS_URL, TAGS_SAVE_AS),
     # ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
     #('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
-    ('Homepage', HOMEPAGE_URL, HOMEPAGE_SAVE_AS),
     ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS)
 )
 # additional menu items
